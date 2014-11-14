655fc8fa5f45f004686d36814781b700553a7adc
James Wang <jw@slickage.com>
2014-11-13 16:05:09 -1000

    Modularized create/find test.

--- test/simple.js
+++ test/simple.js
@@ -4,52 +4,31 @@ var core = require(path.join('..'));
 var config = require(path.join(__dirname, '..', 'config'));
 var recreateSchema = require(path.join(__dirname, '..', 'schema', 'recreate'));
 
+function createAndFind() {
+  return c.users.create({
+    email: 'asdf@asdf.com',
+    username: 'asdf1234',
+    password: 'asdf1234'
+  })
+  .then(function(user) {
+    console.log(user.id);
+    c.users.find(user.id).then(function(user) {
+      console.log('found user');
+      console.log(user);
+    });
+    return user;
+  })
+  .catch(function(err) {
+    console.log('error');
+    console.log(err);
+  });
+}
+
 recreateSchema(function() {
   c = core();
   console.log('Configuration:');
   console.log(config);
-  c.users.create({
-    email: 'asdf@asdf.com',
-    username: 'asdf1234',
-    password: 'asdf1234'
-  })
-  .then(function(user) {
-    console.log(user.id);
-    c.users.find(user.id).then(function(user) {
-      console.log('found user');
-      console.log(user);
-    });
-    // c.users.all().then(function(results) {
-    //   console.log(results);
-    //   pg.end();
-    // });
+  createAndFind().then(function(user) {
     pg.end();
-  })
-  .catch(function(err) {
-    console.log('error');
-    console.log(err);
   });
-
-  c.users.create({
-    email: 'asdf@asdf.com',
-    username: 'asdf1234',
-    password: 'asdf1234'
-  })
-  .then(function(user) {
-    console.log(user.id);
-    c.users.find(user.id).then(function(user) {
-      console.log('found user');
-      console.log(user);
-    });
-    // c.users.all().then(function(results) {
-    //   console.log(results);
-    //   pg.end();
-    // });
-    pg.end();
-  })
-  .catch(function(err) {
-    console.log('error');
-    console.log(err);
-  });
-
 });
