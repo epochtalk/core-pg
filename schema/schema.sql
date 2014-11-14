10ee6930263e9180eabf7cf1c4c9e4ca8e4297b9
James Wang <jw@slickage.com>
2014-11-13 18:17:20 -1000

    Complete-ish schema for boards/threads/posts/users.

--- schema/schema.sql
+++ schema/schema.sql
@@ -5,11 +5,41 @@ CREATE TABLE users (
   username character varying(50) DEFAULT ''::character varying NOT NULL,
   passhash character varying(255) DEFAULT ''::character varying NOT NULL, 
   created_at timestamp without time zone,
-  updated_at timestamp without time zone
+  updated_at timestamp without time zone,
+  imported_at timestamp without time zone
 );
+CREATE UNIQUE INDEX index_users_on_id ON users USING btree (id);
 CREATE UNIQUE INDEX index_users_on_email ON users USING btree (email);
 CREATE UNIQUE INDEX index_users_on_username ON users USING btree (username);
--- INSERT INTO users(email, username, passhash) values('jw@epoch.im', 'wangbus', 'testing1');
--- INSERT INTO users(email, username, passhash) values('julie@epoch.im', 'jsakuda', 'testing2');
--- INSERT INTO users(email, username, passhash) values('ed@epoch.im', 'taesup', 'testing3');
--- INSERT INTO users(email, username, passhash) values('anthony@epoch.im', 'akinsey', 'testing4');
+CREATE TABLE boards (
+  id uuid DEFAULT uuid_generate_v4() NOT NULL,
+  parent_board_id uuid,
+  name character varying(255) DEFAULT ''::character varying NOT NULL,
+  description text DEFAULT '' NOT NULL, 
+  created_at timestamp without time zone,
+  updated_at timestamp without time zone,
+  imported_at timestamp without time zone
+);
+CREATE UNIQUE INDEX index_boards_on_id ON boards USING btree (id);
+CREATE TABLE threads (
+  id uuid DEFAULT uuid_generate_v4() NOT NULL,
+  board_id uuid NOT NULL,
+  created_at timestamp without time zone,
+  updated_at timestamp without time zone,
+  imported_at timestamp without time zone
+);
+CREATE UNIQUE INDEX index_threads_on_id ON threads USING btree (id);
+CREATE INDEX index_threads_on_board_id ON threads USING btree (board_id);
+CREATE TABLE posts (
+  id uuid DEFAULT uuid_generate_v4() NOT NULL,
+  thread_id uuid NOT NULL,
+  user_id uuid NOT NULL,
+  title character varying(255) DEFAULT ''::character varying NOT NULL,
+  body text DEFAULT '' NOT NULL, 
+  created_at timestamp without time zone,
+  updated_at timestamp without time zone,
+  imported_at timestamp without time zone
+);
+CREATE UNIQUE INDEX index_posts_on_id ON posts USING btree (id);
+CREATE INDEX index_posts_on_thread_id ON posts USING btree (thread_id);
+CREATE INDEX index_posts_on_user_id ON posts USING btree (user_id);
