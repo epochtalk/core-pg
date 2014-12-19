ddf0695e039b0ed8f70f782d1254cdfb90ae3f50
Anthony Kinsey <anthony@slickage.com>
2014-12-18 15:06:28 -1000

    Updating data type for timestamps to use 'with time zone'

--- schema/schema.sql
+++ schema/schema.sql
@@ -4,11 +4,11 @@ CREATE TABLE users (
   id serial PRIMARY KEY,
   email character varying(255) NOT NULL,
   username character varying(50) NOT NULL,
-  passhash character varying(255), 
+  passhash character varying(255),
   confirmation_token character varying(255),
-  created_at timestamp without time zone,
-  updated_at timestamp without time zone,
-  imported_at timestamp without time zone
+  created_at timestamp with time zone,
+  updated_at timestamp with time zone,
+  imported_at timestamp with time zone
 );
 
 CREATE UNIQUE INDEX index_users_on_email ON users USING btree (email);
@@ -19,7 +19,7 @@ CREATE TABLE categories (
   id serial PRIMARY KEY,
   name character varying(255) DEFAULT ''::character varying NOT NULL,
   view_order integer,
-  imported_at timestamp without time zone
+  imported_at timestamp with time zone
 );
 
 CREATE TABLE boards (
@@ -28,10 +28,10 @@ CREATE TABLE boards (
   parent_board_id integer,
   category_id integer,
   name character varying(255) DEFAULT ''::character varying NOT NULL,
-  description text DEFAULT '' NOT NULL, 
-  created_at timestamp without time zone,
-  updated_at timestamp without time zone,
-  imported_at timestamp without time zone
+  description text DEFAULT '' NOT NULL,
+  created_at timestamp with time zone,
+  updated_at timestamp with time zone,
+  imported_at timestamp with time zone
   -- FOREIGN KEY (parent_board_id) REFERENCES boards(id),
   -- FOREIGN KEY (category_id) REFERENCES categories(id)
 );
@@ -40,9 +40,9 @@ CREATE TABLE threads (
   -- id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
   id serial PRIMARY KEY,
   board_id integer,
-  created_at timestamp without time zone,
-  updated_at timestamp without time zone,
-  imported_at timestamp without time zone
+  created_at timestamp with time zone,
+  updated_at timestamp with time zone,
+  imported_at timestamp with time zone
   -- FOREIGN KEY (board_id) REFERENCES boards(id)
 );
 
@@ -54,10 +54,10 @@ CREATE TABLE posts (
   thread_id integer,
   user_id integer,
   title character varying(255) DEFAULT ''::character varying NOT NULL,
-  body text DEFAULT '' NOT NULL, 
-  created_at timestamp without time zone,
-  updated_at timestamp without time zone,
-  imported_at timestamp without time zone
+  body text DEFAULT '' NOT NULL,
+  created_at timestamp with time zone,
+  updated_at timestamp with time zone,
+  imported_at timestamp with time zone
   -- FOREIGN KEY (thread_id) REFERENCES threads(id),
   -- FOREIGN KEY (user_id) REFERENCES users(id)
 );
