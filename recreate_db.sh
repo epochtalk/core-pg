72cd1f88d06d5c71bf068b0e734f61c41b17ea47
Bronson Oka <boka@slickage.com>
2014-12-29 18:28:15 -1000

    recreate script takes in database argument

--- recreate_db.sh
+++ recreate_db.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+if [ $# -eq 1 ]
+then
+  database=$1
+else
+  database="epoch_dev"
+fi
 wd=`dirname $BASH_SOURCE`
-dropdb epoch_dev
-createdb epoch_dev
-psql epoch_dev < $wd/schema/schema.sql
-psql epoch_dev < $wd/schema/add_constraints.sql
-psql epoch_dev < $wd/schema/user_schema.sql
-psql epoch_dev < $wd/schema/metadata_schema.sql
+dropdb $database
+createdb $database
+psql $database < $wd/schema/schema.sql
+psql $database < $wd/schema/add_constraints.sql
+psql $database < $wd/schema/user_schema.sql
+psql $database < $wd/schema/metadata_schema.sql
