5c5bf6c8dd9b90032657c6725b13ea0968aa1b1d
Bronson Oka <boka@slickage.com>
2015-03-13 20:21:48 -1000

    add configuration for threads

---
+++ test/configurations/threads.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var fakeInsert = require(path.join(__dirname, '..', 'seed', 'fake-insert'));
+
+// self-reference using a string
+// ex: 'users.0'
+module.exports = {
+  run: [
+    'categories',
+    'boards',
+    'threads'
+  ],
+  methods: {
+    categories: fakeInsert.categories,
+    boards: fakeInsert.boards,
+    threads: fakeInsert.threads
+  },
+  data: {
+    categories: [
+      {},
+      {},
+      {},
+      {}
+    ],
+    boards: [
+      { category_id: 'categories.0.id' }, // has children boards.1,2,3
+      { category_id: 'categories.0.id', parent_board_id: 'boards.0.id' },
+      { category_id: 'categories.0.id', parent_board_id: 'boards.0.id' },
+      { parent_board_id: 'boards.0.id' },
+      {}
+    ],
+    threads: [
+      { board_id: 'boards.0.id' },
+      { board_id: 'boards.0.id' },
+      { board_id: 'boards.0.id' },
+      { board_id: 'boards.1.id' },
+      { board_id: 'boards.1.id' },
+      { board_id: 'boards.1.id' },
+      { board_id: 'boards.2.id' },
+      { board_id: 'boards.2.id' },
+      { board_id: 'boards.2.id' }
+    ]
+  }
+};
