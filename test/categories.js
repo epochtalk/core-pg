a98e7b4a98e4d986a96fe81c5633e69a56d80d35
Bronson Oka <boka@slickage.com>
2015-03-16 18:32:07 -1000

    add categories test file

---
+++ test/categories.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var expect = require('code').expect;
+var Promise = require('bluebird');
+var core = require(path.join(__dirname, '..'))();
+var seed = require(path.join(__dirname, 'seed', 'populate'));
+var fixture = require(path.join(__dirname, 'fixtures', 'categories'));
+
+lab.experiment('Categories', function() {
+  var runtime;
+  lab.before(function(done) {
+    return seed(fixture).then(function(results) {
+      runtime = results;
+      done();
+    });
+  });
+});
