f10ddce53ae52ecf710b9f395805c7346a68c7e0
Bronson Oka <boka@slickage.com>
2015-03-16 15:21:08 -1000

    configurations -> fixtures in populate

--- test/populate.js
+++ test/populate.js
@@ -1,17 +1,17 @@
 var Promise = require('bluebird');
 var Bro = require('brototype').Bro;
 
-module.exports = function(configuration) {
+module.exports = function(fixture) {
   var runtime = {};
-  Object.keys(configuration.data).forEach(function(dataType) {
+  Object.keys(fixture.data).forEach(function(dataType) {
     runtime[dataType] = [];
   });
-  return Promise.each(configuration.run, function(dataType) {
-    return Promise.each(configuration.data[dataType], function(options) {
+  return Promise.each(fixture.run, function(dataType) {
+    return Promise.each(fixture.data[dataType], function(options) {
       Object.keys(options).forEach(function(field) {
         options[field] = Bro(runtime).iCanHaz(options[field]);
       });
-      return configuration.methods[dataType](options).then(function(result) {
+      return fixture.methods[dataType](options).then(function(result) {
         runtime[dataType].push(result);
       });
     });
