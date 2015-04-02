f9368971a7d9b54cc8a6f0d05be9f3844dca3393
Bronson Oka <boka@slickage.com>
2015-04-01 17:37:42 -1000

    threads test instantiates core with opts

--- test/threads.js
+++ test/threads.js
@@ -3,6 +3,7 @@ var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 var expect = require('code').expect;
 var Promise = require('bluebird');
+var core = require(path.join(__dirname, '..'))({host: 'localhost', database: 'epoch_test'});
 var core = require(path.join(__dirname, '..'))();
 var seed = require(path.join(__dirname, 'seed', 'populate'));
 var fixture = require(path.join(__dirname, 'fixtures', 'threads'));
