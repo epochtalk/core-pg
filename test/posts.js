c164e6c716ed7453c913ecec32566b93f9ef4fe1
Bronson Oka <boka@slickage.com>
2015-04-03 17:17:17 -1000

    add test for posts.find

--- test/posts.js
+++ test/posts.js
@@ -10,10 +10,29 @@ var fixture = require(path.join(__dirname, 'fixtures', 'posts'));
 
 lab.experiment('Posts', function() {
   var runtime;
+  var expectations = function(seededPost, post) {
+    expect(post).to.exist;
+    expect(post.thread_id).to.equal(seededPost.thread_id);
+    expect(post.id).to.equal(seededPost.id);
+  };
   lab.before(function(done) {
     return seed(fixture)
     .then(function(results) {
       runtime = results;
+      done();
+    });
+  });
+  lab.test('should find a post by id', function(done) {
+    Promise.map(runtime.posts, function(seededPost) {
+      return core.posts.find(seededPost.id)
+      .then(function(post) {
+        expectations(seededPost, post);
+      })
+      .catch(function(err) {
+        throw(err);
+      });
+    })
+    .then(function() {
       done();
     });
   });
