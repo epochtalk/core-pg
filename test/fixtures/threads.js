297bf1824945b16a6651ae3194d89bfcf11f4bb2
Bronson Oka <boka@slickage.com>
2015-04-03 17:27:28 -1000

    threads tests need seed for users/posts
    now that all the threads tests are running properly, the byBoard query
    is getting executed. the users/posts also need to be seeded for the
    query to execute properly

--- test/fixtures/threads.js
+++ test/fixtures/threads.js
@@ -5,16 +5,26 @@ var fake = require(path.join(__dirname, '..', 'seed', 'fake'));
 // ex: 'users.0'
 module.exports = {
   run: [
+    'users',
     'categories',
     'boards',
-    'threads'
+    'threads',
+    'posts'
   ],
   methods: {
+    users: fake.users,
     categories: fake.categories,
     boards: fake.boards,
-    threads: fake.threads
+    threads: fake.threads,
+    posts: fake.posts
   },
   data: {
+    users: [
+      {},
+      {},
+      {},
+      {}
+    ],
     categories: [
       {},
       {},
@@ -38,6 +48,19 @@ module.exports = {
       { board_id: 'boards.2.id' },
       { board_id: 'boards.2.id' },
       { board_id: 'boards.2.id' }
+    ],
+    posts: [
+      { thread_id: 'threads.0.id', user_id: 'users.0.id' },
+      { thread_id: 'threads.0.id', user_id: 'users.1.id' },
+      { thread_id: 'threads.0.id', user_id: 'users.2.id' },
+      { thread_id: 'threads.1.id', user_id: 'users.0.id' },
+      { thread_id: 'threads.2.id', user_id: 'users.1.id' },
+      { thread_id: 'threads.3.id', user_id: 'users.2.id' },
+      { thread_id: 'threads.4.id', user_id: 'users.0.id' },
+      { thread_id: 'threads.5.id', user_id: 'users.1.id' },
+      { thread_id: 'threads.6.id', user_id: 'users.2.id' },
+      { thread_id: 'threads.7.id', user_id: 'users.0.id' },
+      { thread_id: 'threads.8.id', user_id: 'users.1.id' }
     ]
   }
 };
