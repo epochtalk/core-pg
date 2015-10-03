40b93660b993d0c458433969645e3c28ea3626f3
Anthony Kinsey <anthony@slickage.com>
2015-10-02 16:49:38 -1000

    Slugifying ids and returning ids from db methods.
    * Now returning id when performing an action on the roles to indicate
      a successful transaction.
    
    * Slugifying and deslugifying all role ids.
    
    * Cleaning up unused variables.

--- roles/index.js
+++ roles/index.js
@@ -2,22 +2,20 @@ var roles = {};
 module.exports = roles;
 
 var path = require('path');
-var Promise = require('bluebird');
 var db = require(path.normalize(__dirname + '/../db'));
 var helper = require(path.normalize(__dirname + '/../helper'));
-var NotFoundError = Promise.OperationalError;
-var CreationError = Promise.OperationalError;
-var using = Promise.using;
 
 roles.all = function() {
   var q = 'SELECT id, name, description, lookup, priority, highlight_color, permissions FROM roles';
-  return db.sqlQuery(q);
+  return db.sqlQuery(q)
+  .then(helper.slugify);
 };
 
 roles.update = function(role) {
   var permissions = JSON.stringify(role.permissions);
-  var q = 'UPDATE roles SET permissions = $1 WHERE id = $2';
-  return db.sqlQuery(q, [permissions, role.id]);
+  var q = 'UPDATE roles SET permissions = $1 WHERE id = $2 RETURNING id';
+  return db.sqlQuery(q, [permissions, role.id])
+  .then(helper.slugify);
 };
 
 /* returns user with added role(s) */
@@ -25,18 +23,21 @@ roles.add = function(role) {
   var q, params;
   var permissions = JSON.stringify(role.permissions);
   if (role.id) {
-    q = 'INSERT INTO roles (id, name, description, lookup, priority, highlight_color, permissions, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, now(), now())';
+    q = 'INSERT INTO roles (id, name, description, lookup, priority, highlight_color, permissions, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, now(), now()) RETURNING id';
     params = [role.id, role.name, role.description || '', role.lookup, role.priority, role.highlightColor, permissions];
   }
   else {
-    q = 'INSERT INTO roles (name, description, lookup, priority, highlight_color, permissions, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, now(), now())';
+    q = 'INSERT INTO roles (name, description, lookup, priority, highlight_color, permissions, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, now(), now()) RETURNING id';
     params = [role.name, role.description || '', role.lookup, role.priority, role.highlightColor, permissions];
   }
-  return db.sqlQuery(q, params);
+  return db.scalar(q, params)
+  .then(helper.slugify);
 };
 
 /* returns user with removed role(s) */
 roles.remove = function(roleId) {
-  var q = 'DELETE FROM roles WHERE id = $1';
-  return sqlQuery(q, [roleId]);
+  roleId = helper.deslugify(roleId);
+  var q = 'DELETE FROM roles WHERE id = $1 RETURNING id';
+  return db.scalar(q, [roleId])
+  .then(helper.slugify);
 };
