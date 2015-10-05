8d51abce2bb71007817f61ced0729e47ddd49d75
unenglishable <unenglishable@gmail.com>
2015-10-04 17:14:33 -1000

    implement get for private/all configurations

--- configurations/index.js
+++ configurations/index.js
@@ -24,3 +24,51 @@ configurations.getPublic = function() {
     };
   });
 };
+
+// returns object of private configurations
+configurations.get = function() {
+  var q = 'SELECT * FROM configurations';
+  return db.sqlQuery(q)
+  .then(function(publicConfigurations) {
+    var row = publicConfigurations[0];
+    return {
+      logEnabled: row.log_enabled,
+      privateKey: row.private_key,
+      verfyRegistration: row.verify_registration,
+      loginRequired: row.login_required,
+      website: {
+        title: row.website_title,
+        description: row.website_description,
+        keywords: row.website_keywords,
+        logo: row.website_logo,
+        favicon: row.website_favicon
+      },
+      emailer: {
+        sender: row.emailer_sender,
+        host: row.emailer_host,
+        port: row.emailer_port,
+        user: row.emailer_user,
+        pass: row.emailer_pass,
+        secure: row.emailer_secure
+      },
+      images: {
+        storage: row.images_storage,
+        maxSize: row.images_max_size,
+        expiration: row.images_expiration,
+        interval: row.images_interval,
+        local: {
+          dir: row.images_local_dir,
+          path: row.images_local_path
+        },
+        s3: {
+          root: row.images_s3_root,
+          dir: row.images_s3_dir,
+          bucket: row.images_s3_bucket,
+          region: row.images_s3_region,
+          accessKey: row.images_s3_access_key,
+          secretKey: row.images_s3_secret_key
+        }
+      }
+    };
+  });
+};
