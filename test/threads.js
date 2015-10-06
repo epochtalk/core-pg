3313759d903f35d06bd4a2638a955176982312da
Bronson Oka <boka@slickage.com>
2015-10-05 16:34:52 -1000

    threads and posts tests require core as in other tests

--- test/threads.js
+++ test/threads.js
@@ -4,7 +4,6 @@ var lab = exports.lab = Lab.script();
 var expect = require('code').expect;
 var Promise = require('bluebird');
 var core = require(path.join(__dirname, '..'))({host: 'localhost', database: 'epoch_test'});
-var core = require(path.join(__dirname, '..'))();
 var seed = require(path.join(__dirname, 'seed', 'populate'));
 var fixture = require(path.join(__dirname, 'fixtures', 'threads'));
 var NotFoundError = Promise.OperationalError;
