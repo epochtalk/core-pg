3b81ed49a7f5a390aad9270787b48bf9ff856cf9
Anthony Kinsey <anthony@slickage.com>
2015-10-06 10:53:43 -1000

    Updated roles methods, fixed unused vars in posts
    * Updated roles.all to order by priority for use on the roles
      admin view.
    
    * Slugifying ids in roles.
    
    * Added roles.prioritize which is used to reprioritize all the
      roles.

--- posts/index.js
+++ posts/index.js
@@ -105,13 +105,13 @@ posts.find = function(id) {
 
 posts.byThread = function(threadId, opts) {
   threadId = helper.deslugify(threadId);
-  var columns = 'plist.id, post.thread_id, post.board_id, post.user_id, post.title, post.body, post.raw_body, post.position, post.deleted, post.created_at, post.updated_at, post.imported_at, post.username, post.user_deleted, post.signature, post.avatar, p2.highlight_color';
+  var columns = 'plist.id, post.thread_id, post.board_id, post.user_id, post.title, post.body, post.raw_body, post.position, post.deleted, post.created_at, post.updated_at, post.imported_at, post.username, post.user_deleted, post.signature, post.avatar, p2.highlight_color, p2.role_name';
   var q2 = 'SELECT p.thread_id, t.board_id, p.user_id, p.title, p.body, p.raw_body, p.position, p.deleted, p.created_at, p.updated_at, p.imported_at, u.username, u.deleted as user_deleted, up.signature, up.avatar FROM posts p ' +
     'LEFT JOIN users u ON p.user_id = u.id ' +
     'LEFT JOIN users.profiles up ON u.id = up.user_id ' +
     'LEFT JOIN threads t ON p.thread_id = t.id ' +
     'WHERE p.id = plist.id';
-  var q3 = 'SELECT r.priority, r.highlight_color FROM roles_users ru ' +
+  var q3 = 'SELECT r.priority, r.highlight_color, r.name as role_name FROM roles_users ru ' +
     'LEFT JOIN roles r ON ru.role_id = r.id ' +
     'WHERE post.user_id = ru.user_id ' +
     'ORDER BY priority limit 1';
@@ -138,13 +138,15 @@ var formatPost = function(post) {
     username: post.username,
     deleted: post.user_deleted,
     signature: post.signature,
-    role: post.role,
+    highlight_color: post.highlight_color,
+    role_name: post.role_name
   };
   delete post.user_id;
   delete post.username;
   delete post.user_deleted;
   delete post.signature;
-  delete post.role;
+  delete post.highlight_color;
+  delete post.role_name;
   return post;
 };
 
@@ -176,9 +178,6 @@ posts.pageByUser = function(username, opts) {
 posts.delete = function(id) {
   id = helper.deslugify(id);
   var post;
-  var thread;
-  var board;
-  var user;
   var q;
 
   return using(db.createTransaction(), function(client) {
@@ -205,9 +204,6 @@ posts.delete = function(id) {
 posts.undelete = function(id) {
   id = helper.deslugify(id);
   var post;
-  var thread;
-  var board;
-  var user;
   var q;
 
   return using(db.createTransaction(), function(client) {
--- roles/index.js
+++ roles/index.js
@@ -4,17 +4,21 @@ module.exports = roles;
 var path = require('path');
 var db = require(path.normalize(__dirname + '/../db'));
 var helper = require(path.normalize(__dirname + '/../helper'));
+var Promise = require('bluebird');
+var using = Promise.using;
 
 roles.all = function() {
-  var q = 'SELECT id, name, description, lookup, priority, highlight_color, permissions FROM roles';
+  var q = 'SELECT id, name, description, lookup, priority, highlight_color, permissions FROM roles ORDER BY priority';
   return db.sqlQuery(q)
   .then(helper.slugify);
 };
 
 roles.update = function(role) {
-  var permissions = JSON.stringify(role.permissions);
-  var q = 'UPDATE roles SET permissions = $1 WHERE id = $2 RETURNING id';
-  return db.sqlQuery(q, [permissions, role.id])
+  role.id = helper.deslugify(role.id);
+  role.permissions = JSON.stringify(role.permissions);
+  var q = 'UPDATE roles SET name = $1, description = $2, lookup = $3, priority = $4, highlight_color = $5, permissions = $6, updated_at = now() WHERE id = $7 RETURNING id';
+  var params = [role.name, role.description, role.lookup, role.priority, role.highlightColor, role.permissions, role.id];
+  return db.scalar(q, params)
   .then(helper.slugify);
 };
 
@@ -22,11 +26,11 @@ roles.update = function(role) {
 roles.add = function(role) {
   var q, params;
   var permissions = JSON.stringify(role.permissions);
-  if (role.id) {
+  if (role.id) { // for hardcoded roles with ids, don't slugify id
     q = 'INSERT INTO roles (id, name, description, lookup, priority, highlight_color, permissions, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, now(), now()) RETURNING id';
     params = [role.id, role.name, role.description || '', role.lookup, role.priority, role.highlightColor, permissions];
   }
-  else {
+  else { // for custom roles
     q = 'INSERT INTO roles (name, description, lookup, priority, highlight_color, permissions, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, now(), now()) RETURNING id';
     params = [role.name, role.description || '', role.lookup, role.priority, role.highlightColor, permissions];
   }
@@ -40,4 +44,17 @@ roles.remove = function(roleId) {
   var q = 'DELETE FROM roles WHERE id = $1 RETURNING id';
   return db.scalar(q, [roleId])
   .then(helper.slugify);
+};
+
+roles.reprioritize = function(orderedRoles) {
+  orderedRoles = helper.deslugify(orderedRoles);
+  var q, params;
+  return using(db.createTransaction(), function(client) {
+    q = 'UPDATE roles SET priority = $1 WHERE id = $2';
+    return Promise.map(orderedRoles, function(role) {
+      params = [role.priority, role.id];
+      return client.queryAsync(q, params);
+    });
+  })
+  .then(function() { return {}; });
 };
