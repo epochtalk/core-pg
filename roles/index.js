a4b37a0d7f14b36260e6995e61094ef6d459623c
Anthony Kinsey <anthony@slickage.com>
2015-10-12 12:40:41 -1000

    Added support for searching roles users by username
    * Users can now be searched within a role

--- roles/index.js
+++ roles/index.js
@@ -61,18 +61,32 @@ roles.reprioritize = function(orderedRoles) {
 
 roles.users = function(roleId, opts) {
   roleId = helper.deslugify(roleId);
-  var q = 'SELECT u.id, u.username, u.email FROM users u LEFT JOIN roles_users ru ON u.id = ru.user_id WHERE ru.role_id = $1 ORDER BY username LIMIT $2 OFFSET $3';
+  var q = 'SELECT u.id, u.username, u.email FROM users u LEFT JOIN roles_users ru ON u.id = ru.user_id WHERE ru.role_id = $1';
   opts = opts || {};
   var limit = opts.limit || 25;
   var page = opts.page || 1;
   var offset = (page * limit) - limit;
-  var params = [roleId, limit, offset];
+  var params;
   var userData = {};
+  if (opts && opts.searchStr) {
+    q = [q, 'AND u.username LIKE $2 ORDER BY username LIMIT $3 OFFSET $4'].join(' ');
+    params = [roleId, opts.searchStr + '%', limit, offset];
+  }
+  else {
+    q = [q, 'ORDER BY username LIMIT $2 OFFSET $3'].join(' ');
+    params = [roleId, limit, offset];
+  }
   return db.sqlQuery(q, params)
   .then(function(users) {
     userData.users = users;
-    q = 'SELECT COUNT(u.id) FROM users u LEFT JOIN roles_users ru ON u.id = ru.user_id WHERE ru.role_id = $1';
-    params = [roleId];
+    if (opts && opts.searchStr) {
+      q = 'SELECT COUNT(u.id) FROM users u LEFT JOIN roles_users ru ON u.id = ru.user_id WHERE ru.role_id = $1 AND u.username LIKE $2';
+      params = [roleId, opts.searchStr + '%'];
+    }
+    else {
+      q = 'SELECT COUNT(u.id) FROM users u LEFT JOIN roles_users ru ON u.id = ru.user_id WHERE ru.role_id = $1';
+      params = [roleId];
+    }
     return db.scalar(q, params);
   })
   .then(function(row) {
