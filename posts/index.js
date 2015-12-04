7ecf2df5566ca1c3598f75be4335261913ec6e6d
Edward Kim <ed@slickage.com>
2015-12-03 17:48:18 -1000

    Moderate threads
    This commit adds support for marking threads as moderated so that users
    can delete posts from threads that they own and have marked on creation.
    The thread based methods were also updated to return the 'moderated'
    flag.

--- posts/index.js
+++ posts/index.js
@@ -313,7 +313,38 @@ posts.getThreadFirstPost = function(postId) {
   })
   .then(function(rows) {
     if (rows.length > 0 ) { return rows[0]; }
-    else { throw new NotFoundError('Thread Not Found'); }
+    else { throw new NotFoundError('First Post Not Found'); }
   })
   .then(helper.slugify);
+};
+
+posts.isPostsThreadModerated = function(postId) {
+  postId = helper.deslugify(postId);
+  var q = 'SELECT t.moderated FROM posts p LEFT JOIN threads t ON p.thread_id = t.id WHERE p.id = $1';
+  return db.sqlQuery(q, [postId])
+  .then(function(rows) {
+    if (rows.length > 0 ) { return rows[0]; }
+    else { throw new NotFoundError('Thread Not Found'); }
+  })
+  .then(function(thread) { return thread.moderated; });
+};
+
+posts.isPostsThreadOwner = function(postId, userId) {
+  postId = helper.deslugify(postId);
+  userId = helper.deslugify(userId);
+
+  var q = 'SELECT thread_id FROM posts WHERE id = $1';
+  return db.sqlQuery(q, [postId])
+  .then(function(rows) {
+    if (rows.length > 0) {
+      q = 'SELECT * FROM posts WHERE thread_id = $1 ORDER BY created_at LIMIT 1';
+      return db.sqlQuery(q, [rows[0].thread_id]);
+    }
+    else { throw new NotFoundError('Post Not Found'); }
+  })
+  .then(function(rows) {
+    if (rows.length > 0 ) { return rows[0]; }
+    else { throw new NotFoundError('First Post Not Found'); }
+  })
+  .then(function(post) { return post.user_id === userId; });
 };
