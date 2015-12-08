aa4bee2821ac79f725b79504a668fa72fa97e609
Anthony Kinsey <anthony@slickage.com>
2015-12-08 09:41:59 -1000

    Added support for a mod user id to be passed in to Post Reports
    * Mod user id is used to filter paging of post reports by boards
      that the mod user moderates.
    
    * Updated pagePostReportsCount to accept the moderators users id
      as well.
    
    * TODO: Merge paging and count methods so we only have to make one call

--- reports/index.js
+++ reports/index.js
@@ -393,7 +393,7 @@ reports.updatePostReportNote = function(reportNote) {
 };
 
 reports.pagePostReports = function(opts) {
-  var q = 'SELECT rp.id, rs.status, rp.reporter_user_id, rp.reporter_reason, rp.reviewer_user_id, rp.offender_post_id, rp.created_at, rp.updated_at, (SELECT username FROM users WHERE rp.reporter_user_id = id) as reporter_username, p.created_at as offender_created_at, p.title as offender_title, p.thread_id as offender_thread_id, o.username as offender_author_username, o.id as offender_author_id, o.email as offender_author_email, o.created_at as offender_author_created_at, b.expiration as offender_ban_expiration FROM administration.reports_posts rp JOIN administration.reports_statuses rs ON(rp.status_id = rs.id) JOIN posts p ON(rp.offender_post_id = p.id) JOIN users o ON(p.user_id = o.id) LEFT JOIN (SELECT ub.expiration, ub.user_id FROM users.bans ub WHERE ub.expiration > now()) b ON (o.id = b.user_id)';
+  var q = 'SELECT rp.id, rs.status, rp.reporter_user_id, rp.reporter_reason, rp.reviewer_user_id, rp.offender_post_id, rp.created_at, rp.updated_at, (SELECT username FROM users WHERE rp.reporter_user_id = id) as reporter_username, (SELECT board_id FROM threads t WHERE p.thread_id = t.id), p.created_at as offender_created_at, p.title as offender_title, p.thread_id as offender_thread_id, o.username as offender_author_username, o.id as offender_author_id, o.email as offender_author_email, o.created_at as offender_author_created_at, b.expiration as offender_ban_expiration FROM administration.reports_posts rp JOIN administration.reports_statuses rs ON(rp.status_id = rs.id) JOIN posts p ON(rp.offender_post_id = p.id) JOIN users o ON(p.user_id = o.id) LEFT JOIN (SELECT ub.expiration, ub.user_id FROM users.bans ub WHERE ub.expiration > now()) b ON (o.id = b.user_id)';
   var limit = 10;
   var page = 1;
   var sortField = 'created_at';
@@ -404,9 +404,22 @@ reports.pagePostReports = function(opts) {
   if (opts && opts.sortField) { sortField = opts.sortField; }
   if (opts && opts.sortDesc) { order = 'DESC'; }
   var offset = (page * limit) - limit;
-  if (opts && opts.filter && opts.searchStr) { // filter + search
+  if (opts && opts.modId) { opts.modId = helper.deslugify(opts.modId); } // deslugify modId
+  if (opts && opts.filter && opts.searchStr && opts.modId) { // filter + search + moderated boards
+    q = [q, 'WHERE rs.status = $1 AND o.username LIKE $2 AND (SELECT board_id FROM threads t WHERE p.thread_id = t.id) IN (SELECT board_id FROM board_moderators WHERE user_id = $3) ORDER BY', sortField, order, 'LIMIT $4 OFFSET $5'].join(' ');
+    params = [opts.filter, opts.searchStr + '%', opts.modId, limit, offset];
+  }
+  else if (opts && opts.filter && opts.searchStr && !opts.modId) { // filter + search
     q = [q, 'WHERE rs.status = $1 AND o.username LIKE $2 ORDER BY', sortField, order, 'LIMIT $3 OFFSET $4'].join(' ');
     params = [opts.filter, opts.searchStr + '%', limit, offset];
+  }
+  else if (opts && opts.filter && !opts.searchStr && opts.modId) { // filter + moderated boards
+    q = [q, 'WHERE rs.status = $1 AND (SELECT board_id FROM threads t WHERE p.thread_id = t.id) IN (SELECT board_id FROM board_moderators WHERE user_id = $2) ORDER BY', sortField, order, 'LIMIT $3 OFFSET $4'].join(' ');
+    params = [opts.filter, opts.modId, limit, offset];
+  }
+  else if (opts && !opts.filter && opts.searchStr && opts.modId) { // search + moderated boards
+    q = [q, 'WHERE o.username LIKE $1 AND (SELECT board_id FROM threads t WHERE p.thread_id = t.id) IN (SELECT board_id FROM board_moderators WHERE user_id = $2) ORDER BY', sortField, order, 'LIMIT $3 OFFSET $4'].join(' ');
+    params = [opts.searchStr + '%', opts.modId, limit, offset];
   }
   else if (opts && opts.filter && !opts.searchStr) { // filter only
     q = [q, 'WHERE rs.status = $1 ORDER BY', sortField, order, 'LIMIT $2 OFFSET $3'].join(' ');
@@ -416,7 +429,11 @@ reports.pagePostReports = function(opts) {
     q = [q, 'WHERE o.username LIKE $1 ORDER BY', sortField, order, 'LIMIT $2 OFFSET $3'].join(' ');
     params = [opts.searchStr + '%', limit, offset];
   }
-  else { // no filter or search
+  else if (opts && !opts.filter && !opts.searchStr && opts.modId) { // moderated boards only
+    q = [q, 'WHERE (SELECT board_id FROM threads t WHERE p.thread_id = t.id) IN (SELECT board_id FROM board_moderators WHERE user_id = $1) ORDER BY', sortField, order, 'LIMIT $2 OFFSET $3'].join(' ');
+    params = [opts.modId, limit, offset];
+  }
+  else { // no filter or search or moderated boards
     q = [q, 'ORDER BY', sortField, order, 'LIMIT $1 OFFSET $2'].join(' ');
     params = [limit, offset];
   }
@@ -427,15 +444,32 @@ reports.pagePostReports = function(opts) {
 reports.postReportsCount = function(opts) {
   var q = 'SELECT count(rp.id) FROM administration.reports_posts rp';
   var params;
-  if (opts && opts.status && opts.searchStr) { // filter + search
+  if (opts && opts.modId) { opts.modId = helper.deslugify(opts.modId); } // deslugify modId
+  if (opts && opts.status && opts.searchStr && opts.modId) { // filter + search + moderated boards
+    q += ' JOIN administration.reports_statuses rs ON(rs.id = rp.status_id) JOIN posts p ON(rp.offender_post_id = p.id) JOIN users o ON(p.user_id = o.id) WHERE rs.status = $1 AND o.username LIKE $2 AND (SELECT board_id FROM threads t WHERE p.thread_id = t.id) IN (SELECT board_id FROM board_moderators WHERE user_id = $3)';
+    params = [opts.status, opts.searchStr + '%', opts.modId];
+  }
+  else if (opts && opts.status && opts.searchStr && !opts.modId) { // filter + search
     q += ' JOIN administration.reports_statuses rs ON(rs.id = rp.status_id) JOIN posts p ON(rp.offender_post_id = p.id) JOIN users o ON(p.user_id = o.id) WHERE rs.status = $1 AND o.username LIKE $2';
     params = [opts.status, opts.searchStr + '%'];
   }
-  else if (opts && opts.status && !opts.searchStr) { // filter only
+  else if (opts && opts.status && !opts.searchStr && opts.modId) { // filter + moderated boards
+    q += ' JOIN administration.reports_statuses rs ON(rs.id = rp.status_id) JOIN posts p ON(rp.offender_post_id = p.id) WHERE rs.status = $1 AND (SELECT board_id FROM threads t WHERE p.thread_id = t.id) IN (SELECT board_id FROM board_moderators WHERE user_id = $2)';
+    params = [opts.status, opts.modId];
+  }
+  else if (opts && !opts.status && opts.searchStr && opts.modId) { // search + moderated boards
+    q += ' JOIN posts p ON(rp.offender_post_id = p.id) JOIN users o ON(p.user_id = o.id) WHERE o.username LIKE $1 AND (SELECT board_id FROM threads t WHERE p.thread_id = t.id) IN (SELECT board_id FROM board_moderators WHERE user_id = $2)';
+    params = [opts.searchStr + '%', opts.modId];
+  }
+  else if (opts && !opts.status && !opts.searchStr && opts.modId) { // moderated boards only
+    q += ' JOIN posts p ON(rp.offender_post_id = p.id) WHERE (SELECT board_id FROM threads t WHERE p.thread_id = t.id) IN (SELECT board_id FROM board_moderators WHERE user_id = $1)';
+    params = [opts.modId];
+  }
+  else if (opts && opts.status && !opts.searchStr && !opts.modId) { // filter only
     q += ' JOIN administration.reports_statuses rs ON(rs.id = rp.status_id) WHERE rs.status = $1';
     params = [opts.status];
   }
-  else if (opts && !opts.status && opts.searchStr) { // search only
+  else if (opts && !opts.status && opts.searchStr && !opts.modId) { // search only
     q += ' JOIN posts p ON(rp.offender_post_id = p.id) JOIN users o ON(p.user_id = o.id) WHERE o.username LIKE $1';
     params = [opts.searchStr + '%'];
   }
