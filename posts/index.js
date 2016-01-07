12a008f51cc649c67bc41fb5a083199d1fc75e6b
Edward Kim <ed@slickage.com>
2016-01-06 17:54:14 -1000

    Update to profile posts
    Posts on the profile page now take into account the viewer's priority to
    filter out posts from boards of higher priority. These posts are then
    set as deleted to maintain the page layout.

--- posts/index.js
+++ posts/index.js
@@ -114,7 +114,9 @@ var formatPost = function(post) {
 };
 
 posts.pageByUserCount = function(username) {
-  var q = 'SELECT p.post_count as count FROM users.profiles p JOIN users u ON(p.user_id = u.id) WHERE u.username = $1';
+  var q = 'SELECT p.post_count as count ' +
+  'FROM users.profiles p JOIN users u ON(p.user_id = u.id) ' +
+  'WHERE u.username = $1';
   var params = [username];
   return db.sqlQuery(q, params)
   .then(function(rows) {
@@ -123,16 +125,22 @@ posts.pageByUserCount = function(username) {
   });
 };
 
-posts.pageByUser = function(username, opts) {
-  var q = 'SELECT p.id, p.thread_id, p.user_id, p.title, p.raw_body, p.body, p.position, p.deleted, u.deleted as user_deleted, p.created_at, p.updated_at, p.imported_at, b.id as board_id, exists (SELECT board_id FROM board_mapping WHERE board_id = b.id) as board_visible, (SELECT p2.title FROM posts p2 WHERE p2.thread_id = p.thread_id ORDER BY p2.created_at LIMIT 1) as thread_title FROM posts p LEFT JOIN users u ON p.user_id = u.id LEFT JOIN threads t ON p.thread_id = t.id LEFT JOIN boards b ON t.board_id = b.id WHERE u.username = $1 ORDER BY';
+posts.pageByUser = function(username, priority, opts) {
+  var q = 'SELECT p.id, p.thread_id, p.user_id, p.raw_body, p.body, p.position, p.deleted, u.deleted as user_deleted, p.created_at, p.updated_at, p.imported_at, b.id as board_id, ' + 'EXISTS (SELECT 1 FROM boards WHERE board_id = b.id AND (b.viewable_by >= $2 OR b.viewable_by IS NULL)) as board_visible, ' +
+    '(SELECT p2.title FROM posts p2 WHERE p2.thread_id = p.thread_id ORDER BY p2.created_at LIMIT 1) as thread_title ' +
+    'FROM posts p ' +
+    'LEFT JOIN users u ON p.user_id = u.id ' +
+    'LEFT JOIN threads t ON p.thread_id = t.id ' +
+    'LEFT JOIN boards b ON t.board_id = b.id ' +
+    'WHERE u.username = $1 ORDER BY';
   opts = opts || {};
   var limit = opts.limit || 25;
   var page = opts.page || 1;
   var sortField = opts.sortField || 'created_at';
   var order = opts.sortDesc ? 'DESC' : 'ASC';
   var offset = (page * limit) - limit;
-  q = [q, sortField, order, 'LIMIT $2 OFFSET $3'].join(' ');
-  var params = [username, limit, offset];
+  q = [q, sortField, order, 'LIMIT $3 OFFSET $4'].join(' ');
+  var params = [username, priority, limit, offset];
   return db.sqlQuery(q, params)
   .map(formatPost)
   .then(helper.slugify);
