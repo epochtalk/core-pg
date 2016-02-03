9efdfdc81c2dd7dc87277134905b92e3bf02c3ce
Anthony Kinsey <anthony@slickage.com>
2016-02-02 17:03:31 -1000

    Added ability to page through moderation logs
    * Added position column to moderation logs, this will make paging
      much faster considering the size that this table will grow to.
      There shouldn't be an issue with the position being off since this
      is a log and no records should be deleted. Counting the logs is no
      longer a slow postgres SELECT count(*). Instead count can be
      determined by querying the last log items position.
    
    * Paging and filtering by column and keyword is supported. More advanced
      search features will be added.

--- migrations/sqls/20160126200355-moderation-log-up.sql
+++ migrations/sqls/20160126200355-moderation-log-up.sql
@@ -50,14 +50,24 @@ CREATE TYPE moderation_action_type AS ENUM (
 );
 
 CREATE TABLE moderation_log (
+  position SERIAL PRIMARY KEY,
   mod_username character varying(50) NOT NULL,
   mod_id uuid,
   mod_ip character varying(255),
   action_api_url character varying(2000) NOT NULL,
   action_api_method character varying(25) NOT NULL,
   action_obj json NOT NULL,
-  action_taken_at timestamp with time zone,
+  action_taken_at timestamp with time zone NOT NULL,
   action_type moderation_action_type NOT NULL,
   action_display_text text NOT NULL,
   action_display_url text
 );
+
+CREATE UNIQUE INDEX index_moderation_log_on_position ON moderation_log USING btree (position);
+CREATE INDEX index_moderation_log_on_mod_username ON moderation_log USING btree (mod_username);
+CREATE INDEX index_moderation_log_on_mod_id ON moderation_log USING btree (mod_id);
+CREATE INDEX index_moderation_log_on_mod_ip ON moderation_log USING btree (mod_ip);
+CREATE INDEX index_moderation_log_on_action_api_url ON moderation_log USING btree (action_api_url);
+CREATE INDEX index_moderation_log_on_action_api_method ON moderation_log USING btree (action_api_method);
+CREATE INDEX index_moderation_log_on_action_taken_at ON moderation_log USING btree (action_taken_at);
+CREATE INDEX index_moderation_log_on_action_type ON moderation_log USING btree (action_type);
--- moderation_log/index.js
+++ moderation_log/index.js
@@ -22,3 +22,47 @@ moderationLog.create = function(modLog) {
   ];
   return db.sqlQuery(q, params);
 };
+
+// /api/modlog?page=1&limit=25
+moderationLog.page = function(opts) {
+  var limit = 10;
+  var page = 1;
+  var results, filterCol, filter;
+
+  if (opts && opts.limit) { limit = opts.limit; }
+  if (opts && opts.page) { page = opts.page; }
+  if (opts && opts.filterCol) { filterCol = opts.filterCol; }
+  if (opts && opts.filter) { filter = opts.filter; }
+
+  var q = 'SELECT position AS count FROM moderation_log ORDER BY position DESC LIMIT 1';
+
+  return db.scalar(q)
+  .then(function(row) {
+    var count = row ? row.count : 0;
+    results = {
+      limit: limit,
+      page: page,
+      page_count: Math.ceil(count / limit),
+      filter_col: filterCol || undefined,
+      filter: filter || undefined
+    };
+    q = 'SELECT position, mod_username, mod_id, mod_ip, action_api_url, action_api_method, action_obj, action_taken_at, action_type, action_display_text, action_display_url FROM moderation_log WHERE position > $1';
+
+    var position = (page * limit) - limit;
+    var params;
+    if (filterCol && filter) {
+      q = [q, 'AND ' + filterCol + ' = $2 ORDER BY position LIMIT $3'].join(' ');
+      params = [position, filter, limit];
+    }
+    else {
+      q = [q, 'ORDER BY position LIMIT $2'].join(' ');
+      params = [position, limit];
+    }
+    return db.sqlQuery(q, [position, limit]);
+  })
+  .then(function(logs) {
+    results.data = logs;
+    return results;
+  })
+  .then(helper.slugify);
+};
