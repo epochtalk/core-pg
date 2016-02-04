1ccf5c358089c36e522d744867e087bf17db717e
Bronson Oka <boka@slickage.com>
2016-02-03 18:37:38 -1000

    seed more messages in notifications test fixture

--- test/fixtures/notifications.js
+++ test/fixtures/notifications.js
@@ -20,17 +20,32 @@ module.exports = {
       {}
     ],
     notifications: [
-      { sender_id: 'users.0.id', receiver_id: 'users.0.id' },
-      { sender_id: 'users.0.id', receiver_id: 'users.1.id' },
-      { sender_id: 'users.0.id', receiver_id: 'users.2.id' },
-      { sender_id: 'users.1.id', receiver_id: 'users.0.id' },
-      { sender_id: 'users.1.id', receiver_id: 'users.1.id' },
-      { sender_id: 'users.1.id', receiver_id: 'users.2.id' },
-      { sender_id: 'users.2.id', receiver_id: 'users.0.id' },
-      { sender_id: 'users.2.id', receiver_id: 'users.1.id' },
-      { sender_id: 'users.2.id', receiver_id: 'users.2.id' },
-      { sender_id: 'users.3.id', receiver_id: 'users.0.id' },
-      { sender_id: 'users.3.id', receiver_id: 'users.1.id' }
+      { receiver_id: 'users.0.id', sender_id: 'users.0.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.1.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.2.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.0.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.1.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.2.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.0.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.1.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.2.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.0.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.1.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.2.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.0.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.1.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.2.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.0.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.1.id' },
+      { receiver_id: 'users.0.id', sender_id: 'users.2.id' },
+      { receiver_id: 'users.1.id', sender_id: 'users.0.id' },
+      { receiver_id: 'users.1.id', sender_id: 'users.1.id' },
+      { receiver_id: 'users.1.id', sender_id: 'users.2.id' },
+      { receiver_id: 'users.2.id', sender_id: 'users.0.id' },
+      { receiver_id: 'users.2.id', sender_id: 'users.1.id' },
+      { receiver_id: 'users.2.id', sender_id: 'users.2.id' },
+      { receiver_id: 'users.3.id', sender_id: 'users.0.id' },
+      { receiver_id: 'users.3.id', sender_id: 'users.1.id' }
     ]
   }
 };
