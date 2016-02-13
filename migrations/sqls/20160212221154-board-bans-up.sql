6841512c1b03c72f7f358455cdc440691d843887
Anthony Kinsey <anthony@slickage.com>
2016-02-12 15:45:13 -1000

    Added unique index on user_id and board_id

--- migrations/sqls/20160212221154-board-bans-up.sql
+++ migrations/sqls/20160212221154-board-bans-up.sql
@@ -5,5 +5,6 @@ CREATE TABLE users.board_bans (
 
 CREATE INDEX index_board_bans_on_user_id ON users.board_bans USING btree (user_id);
 CREATE INDEX index_board_bans_on_board_id ON users.board_bans USING btree (board_id);
+CREATE UNIQUE INDEX index_board_bans_on_user_id_and_board_id ON users.board_bans USING btree(user_id, board_id);
 ALTER TABLE users.board_bans ADD CONSTRAINT board_bans_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
 ALTER TABLE users.board_bans ADD CONSTRAINT board_bans_board_id_fk FOREIGN KEY (board_id) REFERENCES boards (id) ON DELETE CASCADE;
