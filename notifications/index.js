6d22f0258d92f87e200687b20e10cf1a1e335802
Bronson Oka <boka@slickage.com>
2016-02-19 17:33:02 -1000

    all notifications queries get only viewed=FALSE

--- notifications/index.js
+++ notifications/index.js
@@ -49,7 +49,7 @@ notifications.dismiss = function(options) {
 notifications.latest = function(user_id, opts) {
   var receiver_id = helper.deslugify(user_id);
 
-  var query = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type = $2 ORDER BY created_at DESC LIMIT $3 OFFSET $4';
+  var query = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type = $2 AND viewed = FALSE ORDER BY created_at DESC LIMIT $3 OFFSET $4';
 
   var type = _.get(opts, 'type');
   var limit = _.get(opts, 'limit', 15);
@@ -67,13 +67,13 @@ notifications.counts = function(user_id) {
   var receiver_id = helper.deslugify(user_id);
   var getNotificationsCount = function(type) {
     // count notifications received by user
-    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type = $2 LIMIT 11';
+    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type = $2 AND viewed = FALSE LIMIT 11';
     return db.sqlQuery(q, [receiver_id, type])
     .then(postProcessCount);
   };
   var getOtherNotificationsCount = function(otherThanTypes) {
     // count notifications received by user
-    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type != ANY ($2) LIMIT 11';
+    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type != ANY ($2) AND viewed = FALSE LIMIT 11';
     return db.sqlQuery(q, [receiver_id, otherThanTypes])
     .then(postProcessCount);
   };
