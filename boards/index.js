62d7bf12bf1f4874477b78c2ed8d15e10832671f
Anthony Kinsey <anthony@slickage.com>
2016-02-26 16:17:39 -1000

    Added a new method in boards. Updated boards.allCategories
    * Added boards.allSelect which retrieves a flat list of all the boards
      and their parent container, whether it be a category or another board.
      This allows the SELECT html element to group by the parent name using
      an optgroup and sort the selections based on the view order that
      appears on the forums front page.
    
    * boards.allCategories now supports opts.stripped. The stripped property
      returns all the boards within their respective categories and parent
      boards, without all the additional metadata such as moderators and
      last post information. This cuts the request times in half for views
      that need the nested board data, but not the additional metadata, such
      as the moderation views for managing bans in boards.

--- boards/index.js
+++ boards/index.js
@@ -14,6 +14,11 @@ boards.all = function() {
   .then(helper.slugify);
 };
 
+boards.allSelect = function() {
+  return db.sqlQuery('SELECT CASE WHEN category_id IS NULL THEN parent_id ELSE category_id END as parent_id, board_id as id, (SELECT name FROM boards WHERE board_id = id), CASE WHEN category_id IS NULL THEN (SELECT name FROM boards WHERE parent_id = id) ELSE (SELECT name FROM categories WHERE category_id = id) END as parent_name, view_order FROM board_mapping')
+  .then(helper.slugify);
+};
+
 boards.create = function(board) {
   board = helper.deslugify(board);
   var q, params;
@@ -157,10 +162,15 @@ boards.allCategories = function(userPriority, opts) {
   .then(function(dbCategories) { categories = dbCategories; })
   // get all board mappings
   .then(function() {
-    return db.sqlQuery('SELECT * FROM ( SELECT b.id, b.name, b.description, b.viewable_by, b.thread_count, b.post_count, b.created_at, b.updated_at, b.imported_at, mb.last_post_username, mb.last_post_created_at, mb.last_thread_id, mb.last_thread_title, mb.last_post_position, bm.parent_id, bm.category_id, bm.view_order FROM board_mapping bm LEFT JOIN boards b ON bm.board_id = b.id LEFT JOIN metadata.boards mb ON b.id = mb.board_id ) blist LEFT JOIN LATERAL ( SELECT p.deleted as post_deleted, u.id as user_id, u.deleted as user_deleted FROM posts p LEFT JOIN users u ON p.user_id = u.id WHERE blist.last_thread_id = p.thread_id ORDER BY p.created_at DESC LIMIT 1 ) p ON true LEFT JOIN LATERAL (SELECT json_agg(row_to_json((SELECT x FROM ( SELECT bm.user_id as id, u.username as username) x ))) as moderators from board_moderators bm LEFT JOIN users u ON bm.user_id = u.id WHERE bm.board_id = blist.id) mods on true');
+    var q = 'SELECT * FROM ( SELECT b.id, b.name, b.description, b.viewable_by, b.thread_count, b.post_count, b.created_at, b.updated_at, b.imported_at, mb.last_post_username, mb.last_post_created_at, mb.last_thread_id, mb.last_thread_title, mb.last_post_position, bm.parent_id, bm.category_id, bm.view_order FROM board_mapping bm LEFT JOIN boards b ON bm.board_id = b.id LEFT JOIN metadata.boards mb ON b.id = mb.board_id ) blist LEFT JOIN LATERAL ( SELECT p.deleted as post_deleted, u.id as user_id, u.deleted as user_deleted FROM posts p LEFT JOIN users u ON p.user_id = u.id WHERE blist.last_thread_id = p.thread_id ORDER BY p.created_at DESC LIMIT 1 ) p ON true LEFT JOIN LATERAL (SELECT json_agg(row_to_json((SELECT x FROM ( SELECT bm.user_id as id, u.username as username) x ))) as moderators from board_moderators bm LEFT JOIN users u ON bm.user_id = u.id WHERE bm.board_id = blist.id) mods on true';
+    if (opts.stripped) {
+      q = 'SELECT b.id, b.name, b.viewable_by, bm.parent_id, bm.category_id, bm.view_order FROM board_mapping bm LEFT JOIN boards b ON bm.board_id = b.id';
+    }
+    return db.sqlQuery(q);
   })
   // handle deleted users
   .then(function(boards) {
+    if (opts.stripped) { return boards; }
     return boards.map(function(board) {
       if (board.post_deleted || board.user_deleted || !board.user_id) {
         board.last_post_username = 'deleted';
