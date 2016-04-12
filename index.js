fa33149c946175cf6358be67ce77a2e0c6f04c02
Edward Kim <ed@slickage.com>
2016-04-11 17:18:59 -1000

    Pulled out reports, updated mod_log enums
    Pulled out all the report based methods into its own module.
    
    The moderation log enums needed to be updated because there is no longer
    a adminReports namespace. All those actions have been moved under the
    reports namespace.

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function core(opts) {
   core.bans = require(path.join(__dirname, 'bans'));
   core.blacklist = require(path.join(__dirname, 'blacklist'));
   core.configurations = require(path.join(__dirname, 'configurations'));
-  core.reports = require(path.join(__dirname, 'reports'));
   core.images = require(path.join(__dirname, 'images'));
   core.moderators = require(path.join(__dirname, 'moderators'));
   core.roles = require(path.join(__dirname, 'roles'));
