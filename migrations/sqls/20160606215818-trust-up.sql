da40db07f603fdfcd23c2e41c3e1d4bd44a1c740
Anthony Kinsey <anthony@slickage.com>
2016-07-15 09:30:19 -1000

    Added table for max depth and trusted boards, inserting default trust

--- migrations/sqls/20160606215818-trust-up.sql
+++ migrations/sqls/20160606215818-trust-up.sql
@@ -29,3 +29,18 @@ CREATE INDEX index_trust_feedback_on_created_at ON trust_feedback USING btree (c
 
 ALTER TABLE trust_feedback ADD CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
 ALTER TABLE trust_feedback ADD CONSTRAINT reporter_id_fk FOREIGN KEY (reporter_id) REFERENCES users (id) ON DELETE CASCADE;
+
+CREATE TABLE trust_max_depth (
+  user_id uuid NOT NULL,
+  max_depth smallint NOT NULL DEFAULT 2
+);
+
+ALTER TABLE trust_max_depth ADD CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
+ALTER TABLE trust_max_depth ADD CONSTRAINT user_id_unique UNIQUE (user_id);
+
+CREATE TABLE trust_boards (board_id uuid NOT NULL);
+
+ALTER TABLE trust_boards ADD CONSTRAINT board_id_fk FOREIGN KEY (board_id) REFERENCES boards (id) ON DELETE CASCADE;
+ALTER TABLE trust_boards ADD CONSTRAINT board_id_unique UNIQUE (board_id);
+
+INSERT INTO users(id, email, username, created_at, updated_at) VALUES ('537d639c-3b50-4545-bea1-8b38accf408e', 'DefaultTrustList@DefaultTrustList', 'DefaultTrustList', now(), now());
