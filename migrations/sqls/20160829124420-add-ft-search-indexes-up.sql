8c2abf139cbf5c43efb0bed00a7b30748d9cb5d6
James Wang <jw@slickage.com>
2016-08-30 17:35:17 -1000

    Finalized tsv index for ft search.

--- migrations/sqls/20160829124420-add-ft-search-indexes-up.sql
+++ migrations/sqls/20160829124420-add-ft-search-indexes-up.sql
@@ -1,11 +1,13 @@
-CREATE MATERIALIZED VIEW fts_index AS 
-SELECT p.id,
-       p.title,
-       p.raw_body,
-       setweight(to_tsvector('simple', p.title), 'A') || 
-       setweight(to_tsvector('simple', p.raw_body), 'B') as document
-FROM posts p
-JOIN users u ON u.id = p.user_id
-GROUP BY p.id, u.id;
+ALTER TABLE posts ADD COLUMN tsv tsvector;
+UPDATE posts SET  
+    tsv = x.tsv
+FROM (  
+    SELECT id,
+           setweight(to_tsvector('simple', COALESCE(title,'')), 'A') ||
+           setweight(to_tsvector('simple', COALESCE(body,'')), 'B')
+           AS tsv
+     FROM posts
+) AS x
+WHERE x.id = posts.id;
 
-CREATE INDEX idx_ft_search ON fts_index USING gin(document);
+CREATE INDEX index_posts_on_tsv ON posts USING gin(tsv);
