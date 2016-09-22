e1e1a065fe6b02f36471e4a31506f0086528375d
Edward Kim <ed@slickage.com>
2016-09-21 17:50:17 -1000

    1.0.4: Remove secrets from DB
    Removed all secrets from the configurations table. Updated the
    configurations/index.js file to account for the fewer columns.
    
    Columns removed: emailer.user, emailer.pass, images.s_3.secret_key, and
    images.s_3.access_key.

--- configurations/index.js
+++ configurations/index.js
@@ -16,7 +16,7 @@ configurations.create = function(options) {
     options.portal.board_id = db.deslugify(options.portal.board_id);
   }
 
-  var q = 'INSERT INTO configurations ("log_enabled", "verify_registration", "login_required", "ga_key", "website.title", "website.description", "website.keywords", "website.logo", "website.favicon", "emailer.sender", "emailer.host", "emailer.port", "emailer.user", "emailer.pass", "emailer.secure", "images.storage", "images.max_size", "images.expiration", "images.interval", "images.local.dir", "images.local.path", "images.s_3.root", "images.s_3.dir", "images.s_3.bucket", "images.s_3.region", "images.s_3.access_key", "images.s_3.secret_key", "rate_limiting.namespace", "rate_limiting.get.interval", "rate_limiting.get.max_in_interval", "rate_limiting.get.min_difference", "rate_limiting.post.interval", "rate_limiting.post.max_in_interval", "rate_limiting.post.min_difference", "rate_limiting.put.interval", "rate_limiting.put.max_in_interval", "rate_limiting.put.min_difference", "rate_limiting.delete.interval", "rate_limiting.delete.max_in_interval", "rate_limiting.delete.min_difference", "portal.enabled", "portal.board_id", "invite_only") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43)';
+  var q = 'INSERT INTO configurations ("log_enabled", "verify_registration", "login_required", "ga_key", "website.title", "website.description", "website.keywords", "website.logo", "website.favicon", "emailer.sender", "emailer.host", "emailer.port", "emailer.secure", "images.storage", "images.max_size", "images.expiration", "images.interval", "images.local.dir", "images.local.path", "images.s_3.root", "images.s_3.dir", "images.s_3.bucket", "images.s_3.region", "rate_limiting.namespace", "rate_limiting.get.interval", "rate_limiting.get.max_in_interval", "rate_limiting.get.min_difference", "rate_limiting.post.interval", "rate_limiting.post.max_in_interval", "rate_limiting.post.min_difference", "rate_limiting.put.interval", "rate_limiting.put.max_in_interval", "rate_limiting.put.min_difference", "rate_limiting.delete.interval", "rate_limiting.delete.max_in_interval", "rate_limiting.delete.min_difference", "portal.enabled", "portal.board_id", "invite_only") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39)';
   var params = [
     options.logEnabled,
     options.verifyRegistration,
@@ -30,8 +30,6 @@ configurations.create = function(options) {
     options.emailer.sender,
     options.emailer.host,
     options.emailer.port,
-    options.emailer.user,
-    options.emailer.pass,
     options.emailer.secure,
     options.images.storage,
     options.images.maxSize,
@@ -43,8 +41,6 @@ configurations.create = function(options) {
     options.images.s3.dir,
     options.images.s3.bucket,
     options.images.s3.region,
-    options.images.s3.accessKey,
-    options.images.s3.secretKey,
     options.rateLimiting.namespace,
     options.rateLimiting.get.interval,
     options.rateLimiting.get.maxInInterval,
@@ -161,14 +157,6 @@ configurations.update = function(options) {
       identifiers.push('"emailer.port"');
       params.push(emailer.port);
     }
-    if (emailer.user !== undefined) {
-      identifiers.push('"emailer.user"');
-      params.push(emailer.user);
-    }
-    if (emailer.pass !== undefined) {
-      identifiers.push('"emailer.pass"');
-      params.push(emailer.pass);
-    }
     if (emailer.secure !== undefined) {
       identifiers.push('"emailer.secure"');
       params.push(emailer.secure);
@@ -220,14 +208,6 @@ configurations.update = function(options) {
       if (s3.region !== undefined) {
         identifiers.push('"images.s_3.region"');
         params.push(s3.region);
-      }
-      if (s3.accessKey !== undefined) {
-        identifiers.push('"images.s_3.access_key"');
-        params.push(s3.accessKey);
-      }
-      if (s3.secretKey !== undefined) {
-        identifiers.push('"images.s_3.secret_key"');
-        params.push(s3.secretKey);
       }
     }
   }
