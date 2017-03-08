4d27b1ea38e0e06c9debf10247f6787aa74d4463
Bronson Oka <boka@slickage.com>
2017-03-07 17:00:03 -1000

    get notifications counts with custom max values
    notifications.counts(user_id, queries);
    
    where a query is
    ```
    { type: <string>, max: <integer> }
    ```

--- notifications/index.js
+++ notifications/index.js
@@ -63,38 +63,43 @@ notifications.latest = function(user_id, opts) {
   .then(helper.slugify);
 };
 
-notifications.counts = function(user_id, opts) {
-  var max = _.get(opts, 'max', 10);
-  var postProcessCount = function(rows) {
-    // if the total rows returned exceeds the max
-    // return string value '{max}+'
-    if (rows.length > max) {
-      return max + '+';
-    }
-    // otherwise, return the count
-    else {
-      return rows.length;
-    }
-  };
+// queries: [
+// ...
+// { type: 'type', max: 999 }
+// ...
+// ]
+notifications.counts = function(user_id, queries) {
   var receiver_id = helper.deslugify(user_id);
-  var getNotificationsCount = function(type) {
+  var getNotificationsCount = function(type, max) {
     // count notifications received by user
-    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type = $2 AND viewed = FALSE LIMIT 11';
-    return db.sqlQuery(q, [receiver_id, type])
-    .then(postProcessCount);
-  };
-  var getOtherNotificationsCount = function(otherThanTypes) {
-    // count notifications received by user
-    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type != ANY ($2) AND viewed = FALSE LIMIT 11';
-    return db.sqlQuery(q, [receiver_id, otherThanTypes])
-    .then(postProcessCount);
+    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type = $2 AND viewed = FALSE LIMIT $3';
+    return db.sqlQuery(q, [receiver_id, type, max + 1])
+    .then(function(rows) {
+      // if the total rows returned exceeds the max
+      // return string value '{max}+'
+      if (rows.length > max) {
+        return max + '+';
+      }
+      // otherwise, return the count
+      else {
+        return rows.length;
+      }
+    })
+    .then(function(count) {
+      return { type, count };
+    });
   };
 
-  return Promise.join(getNotificationsCount('message'), getNotificationsCount('mention'), getOtherNotificationsCount(['message', 'mention']), function(messageNotificationsCount, mentionNotificationsCount, otherNotificationsCount) {
-    return {
-      message: messageNotificationsCount,
-      mention: mentionNotificationsCount,
-      other: otherNotificationsCount
-    };
-  });
+  return Promise.reduce(queries, function(results, query) {
+    var type = _.get(query, 'type');
+    var max = _.get(query, 'max', 10);
+    if (type) {
+      return getNotificationsCount(type, max)
+      .then(function(countForType) {
+        results[countForType.type] = countForType.count;
+        return results;
+      });
+    }
+    else { return; }
+  }, {});
 };
