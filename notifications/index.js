197ebbca16b870d589b580ec0b4e91c9221724f7
Anthony Kinsey <anthony@slickage.com>
2017-03-10 12:37:33 -1000

    Bug fix to make notification counts accurate
    * Count query was always limited to 11 instead of max + 1

--- notifications/index.js
+++ notifications/index.js
@@ -79,14 +79,14 @@ notifications.counts = function(user_id, opts) {
   var receiver_id = helper.deslugify(user_id);
   var getNotificationsCount = function(type) {
     // count notifications received by user
-    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type = $2 AND viewed = FALSE LIMIT 11';
-    return db.sqlQuery(q, [receiver_id, type])
+    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type = $2 AND viewed = FALSE LIMIT $3';
+    return db.sqlQuery(q, [receiver_id, type, max + 1])
     .then(postProcessCount);
   };
   var getOtherNotificationsCount = function(otherThanTypes) {
     // count notifications received by user
-    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type != ANY ($2) AND viewed = FALSE LIMIT 11';
-    return db.sqlQuery(q, [receiver_id, otherThanTypes])
+    var q = 'SELECT * FROM notifications WHERE receiver_id = $1 AND type != ANY ($2) AND viewed = FALSE LIMIT $3';
+    return db.sqlQuery(q, [receiver_id, otherThanTypes, max + 1])
     .then(postProcessCount);
   };
 
