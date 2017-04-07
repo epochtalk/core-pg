55c8aa8317cf729789f88ee0eab2441030393fd1
Anthony Kinsey <anthony@slickage.com>
2017-04-07 13:51:24 -1000

    PG error handling

--- errors.js
+++ errors.js
@@ -1,3 +1,5 @@
+var Promise = require('bluebird');
+
 var errorTemplate = function(name) {
   return function (message, extra) {
     Error.captureStackTrace(this, this.constructor);
@@ -8,8 +10,49 @@ var errorTemplate = function(name) {
 };
 
 var errors = {
+  CreationError: errorTemplate('CreationError'),
+  DeletionError: errorTemplate('DeletionError'),
   NotFoundError: errorTemplate('NotFoundError'),
-  CreationError: errorTemplate('CreationError')
+  ConflictError: errorTemplate('ConflictError'),
+  IntegrityContraintViolationError: errorTemplate('IntegrityContraintViolationError'),
+  RestrictViolationError: errorTemplate('RestrictViolationError'),
+  NotNullViolationError: errorTemplate('NotNullViolationError'),
+  ForeignKeyViolationError: errorTemplate('ForeignKeyViolationError'),
+  UniqueViolationError: errorTemplate('UniqueViolationError'),
+  CheckViolationError: errorTemplate('CheckViolationError'),
+  ExclusionViolationError: errorTemplate('ExclusionViolationError'),
+  handlePgError: handlePgError
 };
+
+function handlePgError(e) {
+  var error;
+  switch (e.code.toString()) {
+    case '23000':
+      error = new errors.IntegrityContraintViolationError(e.detail);
+      break;
+    case '23001':
+      error = new errors.RestrictViolationError(e.detail);
+      break;
+    case '23502':
+      error = new errors.NotNullViolationError(e.detail);
+      break;
+    case '23503':
+      error = new errors.ForeignKeyViolationError(e.detail);
+      break;
+    case '23505':
+      error = new errors.UniqueViolationError(e.detail);
+      break;
+    case '23514':
+      error = new errors.CheckViolationError(e.detail);
+      break;
+    case '23P01':
+      error = new errors.ExclusionViolationError(e.detail);
+      break;
+    default:
+      error = e;
+  }
+
+  throw error;
+}
 
 module.exports = errors;
