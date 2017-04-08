a9afd3bf8fa34e723736652f851a63f73f0a0c40
Bronson Oka <boka@slickage.com>
2017-04-07 14:17:53 -1000

    consolidate postgres error switch into a map

--- errors.js
+++ errors.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var Promise = require('bluebird');
 
 var errorTemplate = function(name) {
@@ -14,44 +15,26 @@ var errors = {
   DeletionError: errorTemplate('DeletionError'),
   NotFoundError: errorTemplate('NotFoundError'),
   ConflictError: errorTemplate('ConflictError'),
-  IntegrityContraintViolationError: errorTemplate('IntegrityContraintViolationError'),
-  RestrictViolationError: errorTemplate('RestrictViolationError'),
-  NotNullViolationError: errorTemplate('NotNullViolationError'),
-  ForeignKeyViolationError: errorTemplate('ForeignKeyViolationError'),
-  UniqueViolationError: errorTemplate('UniqueViolationError'),
-  CheckViolationError: errorTemplate('CheckViolationError'),
-  ExclusionViolationError: errorTemplate('ExclusionViolationError'),
   handlePgError: handlePgError
 };
 
-function handlePgError(e) {
-  var error;
-  switch (e.code.toString()) {
-    case '23000':
-      error = new errors.IntegrityContraintViolationError(e.detail);
-      break;
-    case '23001':
-      error = new errors.RestrictViolationError(e.detail);
-      break;
-    case '23502':
-      error = new errors.NotNullViolationError(e.detail);
-      break;
-    case '23503':
-      error = new errors.ForeignKeyViolationError(e.detail);
-      break;
-    case '23505':
-      error = new errors.UniqueViolationError(e.detail);
-      break;
-    case '23514':
-      error = new errors.CheckViolationError(e.detail);
-      break;
-    case '23P01':
-      error = new errors.ExclusionViolationError(e.detail);
-      break;
-    default:
-      error = e;
-  }
+// map handled Postgres errors to our own custom error types
+function handlePgError(pgError) {
+  // Postgres error codes that we expect to handle
+  var pgErrorMap = {
+    '23000': errorTemplate('IntegrityContraintViolationError'),
+    '23001': errorTemplate('RestrictViolationError'),
+    '23502': errorTemplate('NotNullViolationError'),
+    '23503': errorTemplate('ForeignKeyViolationError'),
+    '23505': errorTemplate('UniqueViolationError'),
+    '23514': errorTemplate('CheckViolationError'),
+    '23P01': errorTemplate('ExclusionViolationError')
+  };
 
+  // map to the correct custom error or return a generic PostgresError
+  var errorType = _.get(pgErrorMap, pgError.code, errorTemplate('PostgresError'));
+  // initialize the error with details and code
+  var error = new errorType(pgError.detail, pgError.code);
   throw error;
 }
 
