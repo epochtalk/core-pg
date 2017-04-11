3f82ca389e8b4c5726562dcb8c9920c5b8c46570
Anthony Kinsey <anthony@slickage.com>
2017-04-10 14:24:20 -1000

    Optimization and allow normal errors through

--- errors.js
+++ errors.js
@@ -1,5 +1,4 @@
 var _ = require('lodash');
-var Promise = require('bluebird');
 
 var errorTemplate = function(name) {
   return function (message, extra) {
@@ -10,32 +9,35 @@ var errorTemplate = function(name) {
   };
 };
 
-var errors = {
-  CreationError: errorTemplate('CreationError'),
-  DeletionError: errorTemplate('DeletionError'),
-  NotFoundError: errorTemplate('NotFoundError'),
-  ConflictError: errorTemplate('ConflictError'),
-  handlePgError: handlePgError
+// Postgres error codes that we expect to handle
+var pgErrorMap = {
+  '23000': errorTemplate('IntegrityContraintViolationError'),
+  '23001': errorTemplate('RestrictViolationError'),
+  '23502': errorTemplate('NotNullViolationError'),
+  '23503': errorTemplate('ForeignKeyViolationError'),
+  '23505': errorTemplate('UniqueViolationError'),
+  '23514': errorTemplate('CheckViolationError'),
+  '23P01': errorTemplate('ExclusionViolationError')
 };
 
 // map handled Postgres errors to our own custom error types
-function handlePgError(pgError) {
-  // Postgres error codes that we expect to handle
-  var pgErrorMap = {
-    '23000': errorTemplate('IntegrityContraintViolationError'),
-    '23001': errorTemplate('RestrictViolationError'),
-    '23502': errorTemplate('NotNullViolationError'),
-    '23503': errorTemplate('ForeignKeyViolationError'),
-    '23505': errorTemplate('UniqueViolationError'),
-    '23514': errorTemplate('CheckViolationError'),
-    '23P01': errorTemplate('ExclusionViolationError')
-  };
+function handlePgError(error) {
+  // If code is not present, error is not a pg error
+  if (!error.code) { throw error; }
 
   // map to the correct custom error or return a generic PostgresError
-  var errorType = _.get(pgErrorMap, pgError.code, errorTemplate('PostgresError'));
+  var ErrorType = _.get(pgErrorMap, error.code, errorTemplate('PostgresError'));
   // initialize the error with details and code
-  var error = new errorType(pgError.detail, pgError.code);
-  throw error;
+  var pgError = new ErrorType(error.detail, error.code);
+  throw pgError;
 }
+
+var errors = {
+  CreationError: errorTemplate('CreationError'),
+  DeletionError: errorTemplate('DeletionError'),
+  NotFoundError: errorTemplate('NotFoundError'), // 404
+  ConflictError: errorTemplate('ConflictError'),
+  handlePgError: handlePgError
+};
 
 module.exports = errors;
