06d10846626b14d6a79a0095dd188949bc81f247
Anthony Kinsey <anthonyjk@gmail.com>
2017-10-31 13:51:14 -1000

    Node-pg promises dont allow chaining of anything but then or catch

--- roles/index.js
+++ roles/index.js
@@ -160,19 +160,23 @@ roles.addRoles = function(usernames, roleId) {
       if (rows.length > 0) { return rows; } // return role names to be mapped
       else { return Promise.reject(); } // users dont exist
     })
-    .map(function(user) { // insert userid and roleid into roles_users if it doesnt exist already
-      q = 'INSERT INTO roles_users(role_id, user_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT 1 FROM roles_users WHERE role_id = $1 AND user_id = $2);';
-      params = [roleId, user.id];
-      return client.query(q, params)
-      .then(function() { // append roles to updated user and return
-        q = 'SELECT roles.* FROM roles_users, roles WHERE roles_users.user_id = $1 AND roles.id = roles_users.role_id';
-        params = [user.id];
-        return client.query(q, params);
-      })
-      .then(function(results) {
-        user.roles = results.rows;
-        return user;
-      });
+    .then(function(users) {
+      return Promise.map(users,
+        function(user) { // insert userid and roleid into roles_users if it doesnt exist already
+          q = 'INSERT INTO roles_users(role_id, user_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT 1 FROM roles_users WHERE role_id = $1 AND user_id = $2);';
+          params = [roleId, user.id];
+          return client.query(q, params)
+          .then(function() { // append roles to updated user and return
+            q = 'SELECT roles.* FROM roles_users, roles WHERE roles_users.user_id = $1 AND roles.id = roles_users.role_id';
+            params = [user.id];
+            return client.query(q, params);
+          })
+          .then(function(results) {
+            user.roles = results.rows;
+            return user;
+          });
+        }
+      );
     }).then(function(allUsers) { return allUsers; });
   })
   .then(helper.slugify);
