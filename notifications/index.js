55d7637f6a70170baeb14db5e1a1cd86feec251a
Bronson Oka <boka@slickage.com>
2019-05-21 14:46:20 -1000

    remove notifications.create

--- notifications/index.js
+++ notifications/index.js
@@ -10,22 +10,6 @@ var using = Promise.using;
 var errors = require(path.normalize(__dirname + '/../errors'));
 var CreationError = errors.CreationError;
 
-notifications.create = function(notification) {
-  notification = helper.deslugify(notification);
-  var q = 'INSERT INTO notifications(sender_id, receiver_id, type, data, created_at) VALUES ($1, $2, $3, $4, now()) RETURNING id, sender_id, receiver_id, created_at, viewed';
-  var params = [_.get(notification, 'sender_id'), _.get(notification, 'receiver_id'), _.get(notification, 'type'), _.get(notification, 'data')];
-  return using(db.createTransaction(), function(client) {
-    return client.query(q, params)
-    .then(function(results) {
-      if (results.rows.length > 0) {
-        notification = results.rows[0];
-      }
-      else { throw new CreationError('Notification Could Not Be Saved'); }
-    });
-  })
-  .then(function() { return helper.slugify(notification); });
-};
-
 notifications.dismiss = function(options) {
   options = helper.deslugify(options);
   var q, params;
