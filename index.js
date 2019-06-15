8ac9247e15c4b0d3898d22542200616f5a0961af
Anthony Kinsey <anthony@slickage.com>
2019-06-14 14:49:28 -1000

    Module system changes

--- index.js
+++ index.js
@@ -6,16 +6,7 @@ function core(opts) {
 
   setup(opts);
 
-  core.bans = require(path.join(__dirname, 'bans'));
-  core.blacklist = require(path.join(__dirname, 'blacklist'));
-  core.configurations = require(path.join(__dirname, 'configurations'));
-  core.images = require(path.join(__dirname, 'images'));
-  core.moderators = require(path.join(__dirname, 'moderators'));
-  core.roles = require(path.join(__dirname, 'roles'));
-  core.polls = require(path.join(__dirname, 'polls'));
   core.notifications = require(path.join(__dirname, 'notifications'));
-  core.moderationLogs = require(path.join(__dirname, 'moderation_logs'));
-  core.userNotes = require(path.join(__dirname, 'user_notes'));
   core.helper = require(path.join(__dirname, 'helper'));
   core.errors = require(path.join(__dirname, 'errors'));
 
